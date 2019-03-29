af82e0cee7d9aa549aa78f63466436153c496fb2
Michael Appuhn <mike.appuhn@gmail.com>
2019-03-29 21:29:10 +0000

    matched some settings that were necessary

--- src/aohistorygroup/settings/base.py
+++ src/aohistorygroup/settings/base.py
@@ -38,7 +38,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_env_variable('SECRET_KEY')
+SECRET_KEY = get_env_variable('DJANGO_SECRET_KEY')
 
 
 # Application definition 
