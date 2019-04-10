08a85c046d03c217d2f380a6e37e7f4f1ccae0f9
Michael Appuhn <mike.appuhn@gmail.com>
2019-04-09 17:56:07 -0700

    tinyMCE support for admin view

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles', 
 	 'audiofield',
+	 'tinymce',
 )
 
 MIDDLEWARE_CLASSES = (
--- django_project/names/models.py
+++ django_project/names/models.py
@@ -2,9 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from tinymce.models import HTMLField 
 
 # Create your models here.
 
 class Name(models.Model):
 	name = models.CharField(max_length=120, primary_key=True)
-	description = models.TextField() 
+	description = HTMLField() 
