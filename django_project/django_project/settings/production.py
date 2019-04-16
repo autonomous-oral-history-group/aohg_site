63f2b9a6328a428bace3d6cd7e024bc4a9442b94
Michael Appuhn <mike.appuhn@gmail.com>
2019-04-15 18:19:06 -0700

    isolated local and production settings, about time!

---
+++ django_project/django_project/settings/production.py
@@ -0,0 +1,163 @@
+"""
+Django settings for django_project project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/topics/settings/
+
+"""
+
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+import netifaces
+from django.core.exceptions import ImproperlyConfigured
+
+def get_env_variable(var_name): 
+    try: 
+        return os.environ[var_name] 
+    except KeyError: 
+        #error_msg = msg % var_name 
+        error_msg = var_name 
+        raise ImproperlyConfigured(error_msg) 
+        pass
+
+
+# Find out what the IP addresses are at run time
+# This is necessary because otherwise Gunicorn will reject the connections
+# Allow Django from all hosts. This snippet is installed from
+# /var/lib/digitalocean/allow_hosts.py
+def ip_addresses():
+    ip_list = []
+    for interface in netifaces.interfaces():
+        addrs = netifaces.ifaddresses(interface)
+        for x in (netifaces.AF_INET, netifaces.AF_INET6):
+            if x in addrs:
+                ip_list.append(addrs[x][0]['addr'])
+    return ip_list
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_env_variable('DJANGO_SECRET_KEY')
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Discover our IP address
+#ALLOWED_HOSTS = ip_addresses()
+#ALLOWED_HOSTS += ['aohistorygroup.com']
+ALLOWED_HOSTS = ['aohistorygroup.com', 'www.aohistorygroup.com',  'aohistorygroup.com', '134.209.59.89', '127.0.0.1']
+ALLOWED_HOSTS += ip_addresses()
+
+APPEND_SLASH = True
+
+# Application definition
+INSTALLED_APPS = (
+    'names.apps.NamesConfig',
+	 'recordings.apps.RecordingsConfig',
+	 'pages.apps.PagesConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles', 
+	 'audiofield',
+	 'tinymce',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware', 
+	 'audiofield.middleware.threadlocals.ThreadLocals',
+)
+
+ROOT_URLCONF = 'django_project.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'django_project.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		  'NAME': get_env_variable('DATABASE_NAME'),
+		  'USER': get_env_variable('DATABASE_USER'),
+		  'PASSWORD' : get_env_variable('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+	("css", os.path.join(BASE_DIR, 'front_end/css')),
+	("assets", os.path.join(BASE_DIR, 'front_end/assets'))
+]
+
+
+#-----
+# Set Following variable
+MEDIA_ROOT = 'media/'
+MEDIA_URL = '/media/'
+
+
+
+# Frontend widget values
+# 0-Keep original, 1-Mono, 2-Stereo
+CHANNEL_TYPE_VALUE = 0
+
+# 0-Keep original, 8000-8000Hz, 16000-16000Hz, 22050-22050Hz,
+# 44100-44100Hz, 48000-48000Hz, 96000-96000Hz
+FREQ_TYPE_VALUE = 8000
+
+# 0-Keep original, 1-Convert to MP3, 2-Convert to WAV, 3-Convert to OGG
+CONVERT_TYPE_VALUE = 0
