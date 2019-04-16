9b050326dc0ab97e1f3ed246cffc0fa4cc4a4552
Michael Appuhn <mike.appuhn@gmail.com>
2019-04-15 18:23:26 -0700

    changed the relative paths of settings files, since they were moved further into a directory

--- django_project/django_project/settings/local.py
+++ django_project/django_project/settings/local.py
@@ -86,7 +86,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, '../templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,11 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-	("css", os.path.join(BASE_DIR, 'front_end/css')),
-	("assets", os.path.join(BASE_DIR, 'front_end/assets'))
+	("css", os.path.join(BASE_DIR, '../front_end/css')),
+	("assets", os.path.join(BASE_DIR, '../front_end/assets'))
 ]
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
--- django_project/django_project/settings/production.py
+++ django_project/django_project/settings/production.py
@@ -136,10 +136,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-	("css", os.path.join(BASE_DIR, 'front_end/css')),
+	("css", os.path.join(BASE_DIR, '../front_end/css')),
 	("assets", os.path.join(BASE_DIR, 'front_end/assets'))
 ]
 
