19fa099e80cfb55f2671b18ab04da1cd89ed4721
Michael Appuhn <mike.appuhn@gmail.com>
2019-04-18 17:17:48 -0700

    basic list and detail views for pages

--- django_project/pages/views.py
+++ django_project/pages/views.py
@@ -2,5 +2,20 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Page
+
 
 # Create your views here.
+
+"""
+class PageList(ListView):
+	model = Name
+	template_name = 'pages/page_list.html'
+	context_object_name = 'page'
+"""
+
+class PageDetail(DetailView):
+	model = Page
+	template_name = 'pages/page_detail.html'
+	context_object_name = 'page' 
