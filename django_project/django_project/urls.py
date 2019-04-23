e761bde5be645415ca640ca57fed16d0b71a5785
Michael Appuhn <mike.appuhn@gmail.com>
2019-04-22 21:09:22 -0700

    added basic media file to sidebar

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -17,12 +17,16 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from names.views import NamesList
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
    url(r'^admin/', admin.site.urls),
 	url(r'^tinymce/', include('tinymce.urls')),
 	#url(r'^names', NamesList),
 	url(r'^names/', include('names.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#https://docs.djangoproject.com/en/1.11/howto/static-files/#serving-files-uploaded-by-a-user-during-development
 
 
 
