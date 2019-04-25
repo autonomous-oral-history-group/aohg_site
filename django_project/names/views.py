cad288dd85e17e81cef3b7de38d2f8dc73c74db6
Michael Appuhn <mike.appuhn@gmail.com>
2019-04-24 17:57:59 -0700

    added sidebar to names list template

--- django_project/names/views.py
+++ django_project/names/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Name
+from sidebars.models import Sidebar
 
 
 # Create your views here.
@@ -12,6 +13,16 @@ class NamesList(ListView):
 	model = Name
 	template_name = 'names/name_list.html'
 	context_object_name = 'names'
+
+	def get_sidebar_general_page(self):
+		return Sidebar.objects.get(title='General')
+
+	def get_context_data(self, **kwargs):
+		context = super(NamesList, self).get_context_data(**kwargs) 
+		context['sidebar'] = self.get_sidebar_general_page()
+		return context
+
+
 
 class NameDetail(DetailView):
 	model = Name
