891aefe257b5802be408f2c157907f4f868b6919
Mikey Appuhn <mike.appuhn@gmail.com>
2019-04-28 09:55:29 -0700

    improved the url of base, added subjects to navigation

--- django_project/names/urls.py
+++ django_project/names/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import NamesList, NameDetail, SubjectList, SubjectDetail
 
 urlpatterns=[
-	url(r'^name$', NamesList.as_view(), name='names_list'), 
+	url(r'^name(/)?$', NamesList.as_view(), name='names_list'), 
 	url(r'^name/(?P<slug>[\w-]+)$', NameDetail.as_view(), name='name_detail'), 
 	url(r'^subject$', SubjectList.as_view(), name='subject_list'), 
 	url(r'^subject/(?P<slug>[\w-]+)$', SubjectDetail.as_view(), name='subject_detail'), 
