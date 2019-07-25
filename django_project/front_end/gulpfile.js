0a7c9f6e3433640764c3213e8ca60bb31d50f4d4
Mikey Appuhn <mike.appuhn@gmail.com>
2019-07-25 00:18:25 -0700

    added a gulpfile for browserify-based workflow

--- django_project/front_end/gulpfile.js
+++ django_project/front_end/gulpfile.js
@@ -3,6 +3,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var browserify = require('browserify');
+
+//var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var log = require('gulplog');
+
+var tap = require('gulp-tap');
+
  
 sass.compiler = require('node-sass');
  
@@ -17,3 +27,33 @@ gulp.task('watch', function () {
   gulp.watch('./src/styles/**/*.scss', ['sass']);
 });
 
+
+//https://github.com/gulpjs/gulp/blob/master/docs/recipes/browserify-with-globs.md
+gulp.task('js', function () {
+	return gulp.src('app/**/*.js', {read: false})
+    // transform file objects using gulp-tap plugin
+    .pipe(tap(function (file) {
+
+      log.info('bundling ' + file.path);
+
+      // replace file contents with browserify's bundle stream
+      file.contents = browserify(file.path, {debug: true}).bundle();
+    }))
+
+
+
+    // transform streaming contents into buffer contents (because gulp-sourcemaps does not support streaming contents)
+    .pipe(buffer())
+
+    // load and init sourcemaps
+    .pipe(sourcemaps.init({loadMaps: true}))
+
+    .pipe(uglify())
+
+    // write sourcemaps
+    .pipe(sourcemaps.write('./'))
+
+    .pipe(gulp.dest('js'));
+
+	 
+});
