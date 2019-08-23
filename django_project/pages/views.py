6ba26da964c487bef72c237c5d6e0ad119dbf36e
Michael Appuhn <mike.appuhn@gmail.com>
2019-08-22 22:11:23 -0700

    added initial sample form code

--- django_project/pages/views.py
+++ django_project/pages/views.py
@@ -37,3 +37,16 @@ class Index(TemplateView):
 		return context
 
 
+from .forms import RequestForm
+from django.views.generic.edit import FormView
+
+class RequestView(FormView):
+    template_name = 'contact.html'
+    form_class = RequestForm
+    success_url = '/thanks/'
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.send_email()
+        return super(ContactView, self).form_valid(form)
